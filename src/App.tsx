6950330898384d706475e85d341caa4459030f04
keithjasperquevada <keithjasper.quevada@gmail.com>
2026-08-08 00:32:13 +0800

    Topbar component (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import BottomNav from "./components/navigation/BottomNavigationBar";
+import TopBar from "./components/navigation/TopBar";
 
 
 function App() {
   return (
     <>
+      <div className="container-fluid px-4 py-3">
+        <TopBar />
+      </div>
+      
       <BottomNav />
     </>   
   );
