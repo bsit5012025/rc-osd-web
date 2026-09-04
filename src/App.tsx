2ffd08584938870e9db245a9fb22acd7073591da
Keith Jasper Quevada <keithjasper.quevada@gmail.com>
2026-09-04 19:07:44 +0800

    Update admin UI

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,7 @@ import FileAppealPage from "./pages/student/appeal/fileAppealPage";
 import DeptHeadDashboardPage from "./pages/deptHead/dashboard/deptHeadDashboardPage";
 import DeptHeadProfilePage from "./pages/deptHead/profile/deptHeadProfilePage";
 import DeptHeadRequestPage from "./pages/deptHead/request/deptHeadRequestPage";
-import AppLayout from "./components/layout/AppLayout";
-import DeptHeadLayout from "./components/layout/DeptHeadLayout";
-import AdminLayout from "./components/layout/AdminLayout";
 import AdminDashboardPage from "./pages/admin/dashboard/adminDashboardPage";
-import AdminOffensePage from "./pages/admin/offense/adminOffensePage";
-import AdminStudentPage from "./pages/admin/student/adminStudentPage";
 import AppLayout from "./components/layout/AppLayout";
 import DeptHeadLayout from "./components/layout/DeptHeadLayout";
 import AdminLayout from "./components/layout/AdminLayout";
@@ -94,16 +89,6 @@ function App() {
                     <Route
                         path="/admin/dashboard"
                         element={<AdminDashboardPage />}
-                    />
-
-                    <Route
-                        path="/admin/offenses"
-                        element={<AdminOffensePage />}
-                    />
-
-                    <Route
-                        path="/admin/students"
-                        element={<AdminStudentPage />}
                     />
 
                 </Route>
--- src/components/layout/AdminLayout.tsx
+++ src/components/layout/AdminLayout.tsx
@@ -8,16 +8,6 @@ const adminNavItems: SidebarNavItem[] = [
         icon: "bi-grid-fill",
         path: "/admin/dashboard",
     },
-    {
-        name: "Student",
-        icon: "bi-people-fill",
-        path: "/admin/students",
-    },
-    {
-        name: "Offenses",
-        icon: "bi-exclamation-triangle-fill",
-        path: "/admin/offenses",
-    },
 ];
 
 export default function AdminLayout() {
